fb4502239debdfd3682577a0dec8b8464c5e1283
Deneo <137724296+NeoDS009@users.noreply.github.com>
2024-01-29 12:34:44 -0500

    Delete A004- GUI_Electronic_Store_Application directory

--- A004- GUI_Electronic_Store_Application/src/main/java/assignment4/StoreViewUpdate.java
+++
@@ -1,7 +0,0 @@
-
-package assignment4;
-
-// an interface: those who implements this will have to also use the update() method
-public interface StoreViewUpdate {
-    public void update(ElectronicStore model);
-}
