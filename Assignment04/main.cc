f8613a9b5afb7858ed5ec863c436a5a14d91a71b
Val <137724296+NeoDS009@users.noreply.github.com>
2024-05-02 19:56:39 -0400

    Add files via upload

---
+++ Assignment04/main.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "defs.h"
+#include "Escape.h"
+#include "Hero.h"
+#include "PartArray.h"
+#include "Pit.h"
+#include "Snorc.h"
+#include "Position.h"
+#include "Participant.h"
+
+
+int main()
+{
+    Escape myEscape;
+    myEscape.runEscape();
+}
